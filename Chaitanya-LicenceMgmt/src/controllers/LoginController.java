960e4c4fa32fa23453d5bff4595d1bf6cbbc3e21
abhijeetpoojari <abhijeetpoojari@gmail.com>
2016-04-07 11:18:57 +0530

    Created Setup for the Project

---
+++ Chaitanya-LicenceMgmt/src/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+ 
+public class LoginController extends AbstractController{
+ 
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+		HttpServletResponse response) throws Exception {
+ 
+		ModelAndView modelandview = new ModelAndView("index");
+		modelandview.addObject("welcomeMessage", "Hi User, welcome to the first Spring MVC Application");
+		
+		return modelandview;
+	}
+
+	
+}
